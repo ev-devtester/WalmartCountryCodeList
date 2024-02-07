ae5841c5f444256a6ac38ab2bd5cd20ea5cb40e6
EricVBDev <89611257+EricVBDev@users.noreply.github.com>
2024-02-07 15:32:29 -0600

    Initial commit

--- app/src/main/java/com/example/walmartcountryinfo/model/CountryResponse.kt
+++
@@ -1,9 +0,0 @@
-package com.example.walmartcountryinfo.model
-
-data class CountryResponse(
-    val name: String,
-    val code: String,
-    val region: String,
-    val capital: String
-)
-
--- app/src/main/java/com/example/walmartcountryinfo/model/UIState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.walmartcountryinfo.model
-
-sealed class UIState  {
-    object Loading: UIState()
-    class Error(val error: Exception): UIState()
-    class Success<T>(val response: T): UIState()
-}
